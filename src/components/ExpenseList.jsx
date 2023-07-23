abf1738279a94c81dc3b0699b98dd977501f05c3
benamager <benaprivat@gmail.com>
2023-07-23 22:03:52 +0200

    Add search functionality

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,11 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
-import ExpensesContext from "@/contexts/Expenses";
+import { useState, useEffect } from "react";
 import { LeadingActions, SwipeableList, SwipeableListItem, SwipeAction, TrailingActions } from "react-swipeable-list";
 import "react-swipeable-list/dist/styles.css";
 import groupExpensesByDay from "@/utils/groupExpensesByDay";
 import useAddExpense from "@/hooks/useDeleteExpense";
-import useFindCategory from "@/hooks/useFindCategory";
 import { BsFillInfoSquareFill } from "react-icons/bs";
 
 function editAction(navigate, expense) {
@@ -29,11 +27,9 @@ function deleteAction(expenseId, deleteExpense) {
   );
 }
 
-export default function ExpenseList() {
+export default function ExpenseList({ expenses, isSearching }) {
   const navigate = useNavigate();
-  const { findCategory } = useFindCategory();
 
-  const { expenses } = useContext(ExpensesContext);
   const [groupedExpenses, setGroupedExpenses] = useState([]);
   const { deleteExpense } = useAddExpense();
   const [isLoading, setIsLoading] = useState(true);
@@ -45,13 +41,23 @@ export default function ExpenseList() {
   }, [expenses]);
 
   return groupedExpenses.length === 0 && !isLoading ? (
-    <div className="text-center flex flex-col items-center mt-11 mx-1">
-      <BsFillInfoSquareFill size="30px" className="mx-auto text-slate-300 mb-4" />
-      <span className="text-lg mb-1">No expenses yet</span>
-      <span className="text-slate-500 font-light">Add some by pressing the plus icon</span>
-    </div>
+    <>
+      {isSearching ? (
+        <div className="text-center flex flex-col items-center mt-11 mx-1">
+          <BsFillInfoSquareFill size="30px" className="mx-auto text-slate-300 mb-4" />
+          <span className="text-lg mb-1">No search results</span>
+          <span className="text-slate-500 font-light">Try searching for something else.</span>
+        </div>
+      ) : (
+        <div className="text-center flex flex-col items-center mt-11 mx-1">
+          <BsFillInfoSquareFill size="30px" className="mx-auto text-slate-300 mb-4" />
+          <span className="text-lg mb-1">No expenses yet</span>
+          <span className="text-slate-500 font-light">Add some by pressing the plus icon</span>
+        </div>
+      )}
+    </>
   ) : (
-    <ul className="mx-4 text-sm flex flex-col-reverse">
+    <ul className="mx-4 text-sm flex flex-col-reverse mt-3">
       {groupedExpenses.map((group) => (
         <li key={group.day} className="mb-11">
           <div className="border-b border-b-slate-100 flex justify-between text-slate-300 ml-11 pb-1">
@@ -60,14 +66,12 @@ export default function ExpenseList() {
           </div>
           <SwipeableList className="flex flex-col-reverse">
             {group.expenses.map((expense) => {
-              const category = findCategory(expense.categoryId);
-
               return (
                 <SwipeableListItem key={expense.id} leadingActions={editAction(navigate, expense)} trailingActions={deleteAction(expense.id, deleteExpense)} className="flex items-center mt-2 py-1">
-                  <img className="w-6 h-6 mr-5" src={category?.iconUrl} alt="Image of emoji" />
+                  <img className="w-6 h-6 mr-5" src={expense?.category?.iconUrl} alt="Image of emoji" />
                   <div className="flex flex-col">
                     <span>{expense.title ? `${expense.title.charAt(0).toUpperCase() + expense.title.slice(1)}` : "No title"}</span>
-                    <span className="text-slate-300">{category?.name ? category?.name : "No category"}</span>
+                    <span className="text-slate-300">{expense?.category?.name ? expense?.category?.name : "No category"}</span>
                   </div>
                   <span className="ml-auto">{expense.price} DKK</span>
                 </SwipeableListItem>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,38 @@
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AiFillPlusCircle } from "react-icons/ai";
 import ExpenseList from "@/components/ExpenseList";
 import SpentHeading from "@/components/SpentHeading";
 import useExpenseData from "@/hooks/useExpenseData";
+import ExpensesContext from "@/contexts/Expenses";
+import useFindCategory from "@/hooks/useFindCategory";
+import useInput from "@/hooks/useInput";
+
+function searchItems(expenses, query) {
+  return expenses.filter((expense) => expense.title.toLowerCase().includes(query.toLowerCase()) || expense.price.toString() === query || expense.category.name.toLowerCase().includes(query.toLowerCase()));
+}
 
 export default function Homepage() {
   const { totalForPeriod } = useExpenseData();
+  const { expenses } = useContext(ExpensesContext);
+  const { findCategory } = useFindCategory();
+  const { input: searchInput, inputValue: searchQuery } = useInput({
+    placeholder: "Search by name, price and category",
+    className: "w-full py-1 outline-none focus:bg-slate-100 rounded-md text-lg transform scale-[80%] text-right pr-2 ml-auto max-w-[344px] translate-x-6 text-black placeholder-slate-300",
+  });
+
+  const expensesWithCategory = expenses.map((expense) => {
+    return {
+      ...expense,
+      category: findCategory(expense.categoryId),
+    };
+  });
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    setSearchResults(searchItems(expensesWithCategory, searchQuery));
+  }, [searchQuery, expenses]);
 
   return (
     <>
@@ -13,7 +40,8 @@ export default function Homepage() {
         <AiFillPlusCircle size="35px" className="p-3 box-content ml-auto" />
       </Link>
       <SpentHeading hasTitle={true} amount={totalForPeriod} />
-      <ExpenseList />
+      {expensesWithCategory.length != 0 && <>{searchInput}</>}
+      <ExpenseList expenses={searchQuery.length < 1 ? expensesWithCategory : searchResults} isSearching={searchQuery.length != 0} />
     </>
   );
 }
