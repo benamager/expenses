ddd6a3d28d5634c2e6678589d6e4adb20daea547
benamager <benaprivat@gmail.com>
2023-07-23 22:08:43 +0200

    Return dummy no category if none found

--- src/hooks/useFindCategory.jsx
+++ src/hooks/useFindCategory.jsx
@@ -1,12 +1,23 @@
 import { useContext } from "react";
 import CategoriesContext from "../contexts/Categories";
+import { nanoid } from "nanoid";
 
 export default function useFindCategory() {
   const { categories } = useContext(CategoriesContext);
 
   // find category by id
   function findCategory(categoryId) {
-    return categories.find((c) => c.id === categoryId);
+    let category = categories.find((c) => c.id === categoryId);
+
+    if (!category) {
+      category = {
+        id: nanoid(),
+        name: "No category",
+        iconUrl: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f937-200d-2642-fe0f.png",
+      };
+    }
+
+    return category;
   }
 
   // return the modal and the state setter function
