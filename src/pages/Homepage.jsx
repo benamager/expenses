ac06b4b132e46b45ed4f5490bf55d6270dbcc23d
benamager <benaprivat@gmail.com>
2023-10-15 23:41:16 +0200

    Fix add expense btn positioning

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,39 +9,39 @@ import useFindCategory from "@/hooks/useFindCategory";
 import useInput from "@/hooks/useInput";
 
 function searchItems(expenses, query) {
-  return expenses.filter((expense) => expense.title.toLowerCase().includes(query.toLowerCase()) || expense.price.toString() === query || expense.category.name.toLowerCase().includes(query.toLowerCase()));
+    return expenses.filter((expense) => expense.title.toLowerCase().includes(query.toLowerCase()) || expense.price.toString() === query || expense.category.name.toLowerCase().includes(query.toLowerCase()));
 }
 
 export default function Homepage() {
-  const { totalForPeriod } = useExpenseData();
-  const { expenses } = useContext(ExpensesContext);
-  const { findCategory } = useFindCategory();
-  const { input: searchInput, inputValue: searchQuery } = useInput({
-    placeholder: "Search by name, price or category",
-    className: "w-full py-1 outline-none focus:bg-slate-100 rounded-md text-lg transform scale-[80%] text-right pr-2 ml-auto max-w-[344px] translate-x-6 text-black placeholder-slate-300",
-  });
+    const { totalForPeriod } = useExpenseData();
+    const { expenses } = useContext(ExpensesContext);
+    const { findCategory } = useFindCategory();
+    const { input: searchInput, inputValue: searchQuery } = useInput({
+        placeholder: "Search by name, price or category",
+        className: "w-full py-1 outline-none focus:bg-slate-100 rounded-md text-lg transform scale-[80%] text-right pr-2 ml-auto max-w-[344px] translate-x-6 text-black placeholder-slate-300",
+    });
 
-  const expensesWithCategory = expenses.map((expense) => {
-    return {
-      ...expense,
-      category: findCategory(expense.categoryId),
-    };
-  });
+    const expensesWithCategory = expenses.map((expense) => {
+        return {
+            ...expense,
+            category: findCategory(expense.categoryId),
+        };
+    });
 
-  const [searchResults, setSearchResults] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    setSearchResults(searchItems(expensesWithCategory, searchQuery));
-  }, [searchQuery, expenses]);
+    useEffect(() => {
+        setSearchResults(searchItems(expensesWithCategory, searchQuery));
+    }, [searchQuery, expenses]);
 
-  return (
-    <>
-      <Link to="expense" className="ml-auto fixed right-0 z-50">
-        <AiFillPlusCircle size="40px" className="p-3 box-content ml-auto drop-shadow-md " />
-      </Link>
-      <SpentHeading hasTitle={true} amount={totalForPeriod} className="mt-[59px]" />
-      {expensesWithCategory.length != 0 && <>{searchInput}</>}
-      <ExpenseList expenses={searchQuery.length < 1 ? expensesWithCategory : searchResults} isSearching={searchQuery.length != 0} />
-    </>
-  );
+    return (
+        <div>
+            <Link to="expense" className="ml-auto sticky right-0 top-0 z-50 block">
+                <AiFillPlusCircle size="40px" className="p-3 box-content ml-auto drop-shadow-md " />
+            </Link>
+            <SpentHeading hasTitle={true} amount={totalForPeriod} className="mt-[20px]" />
+            {expensesWithCategory.length != 0 && <>{searchInput}</>}
+            <ExpenseList expenses={searchQuery.length < 1 ? expensesWithCategory : searchResults} isSearching={searchQuery.length != 0} />
+        </div>
+    );
 }
